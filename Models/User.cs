fa0391f90c6908d17a08fabe5637cd608b45e8fb
Aboda7m <a20092254@gmail.com>
2024-10-22 19:39:34 +0300

    gg

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,11 @@
-// Models/User.cs
 namespace Mr_XL_Graduation.Models
 {
     public class User
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string StudentId { get; set; } // New field
+        public string FullName { get; set; } // Optional field
+        // Add more fields as necessary
     }
 }
